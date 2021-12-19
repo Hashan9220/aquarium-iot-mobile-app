f8f2d22cba3bcb7382de299c996baaae49f1443b
SachiniUmayangana <sachi.umay@gmail.com>
2021-12-19 21:26:01 +0530

    feed screen ui continued

--- views/FeedScreen.js
+++ views/FeedScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useState } from 'react';
 import {Text, StyleSheet, Image} from 'react-native';
 import LinearGradient from "react-native-linear-gradient";
 import database from '@react-native-firebase/database';
@@ -6,10 +6,23 @@ import { Button } from 'react-native-paper';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import * as Animatable from 'react-native-animatable';
 
-export default function FeedScreen(){
-    const setAnimatedImage = () => {
-      return
-       <Image source={require('../assets/icons/cvs.gif')} />
+export default function FeedScreen () {
+
+    const [imgOpacity, setImgOpacity] = useState(0);
+    const [btnColor, setBtnColor] = useState("#FFF")
+    const [text, setText] = useState("FEED")
+
+   const toggleImage = () => {
+        console.log("inside");
+        if ( imgOpacity === 0 ) {
+            setImgOpacity(1)
+            setBtnColor("#dfe6e9")
+            setText("STOP")
+        } else {
+            setImgOpacity(0)
+            setBtnColor("#FFF")
+            setText("FEED")
+        }
     }
 
     return(
@@ -17,16 +30,20 @@ export default function FeedScreen(){
             colors={['#a6d4ff', '#1E90FF']}
             style={styles.container}
         >
-            <Button style={styles.feedBtn} mode="contained" onPress={() => console.log('Pressed')}>
-                <Text style={styles.feedTxt}>Feed</Text>
+
+            <Image style={{marginLeft: '20%'}} source={require('../assets/icons/20170.gif')} />
+
+            <Button style={{backgroundColor: btnColor, width: '60%'}} mode="contained"  onPress={() => {
+                toggleImage()
+            }}>
+                <Text style={{color: '#000'}}>{text}</Text>
             </Button>
 
             <Animatable.View animation="fadeInRight">
-            <Image source={require('../assets/icons/ZKZl.gif')} />
+            <Image style={{opacity: imgOpacity, marginLeft: '-50%'}} source={require('../assets/icons/giphy.gif')} />
             </Animatable.View>
 
         </LinearGradient>
-
     )
 }
 
@@ -36,14 +53,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    feedBtn: {
-        backgroundColor: 'white',
-        width: '50%',
-        height: '6%',
-    },
+    // feedBtn: {
+    //     backgroundColor: btnColor,
+    //     width: '50%',
+    //     height: '6%',
+    // },
     feedTxt: {
         color: '#1E90FF',
         fontWeight: 'bold',
+    },
+    image: {
+
     }
 
 })
